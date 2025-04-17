0f633448d88ea9067788d0f18db3e999e476ff24
Qi'ra <178964539+redrubyyy611@users.noreply.github.com>
2025-04-16 23:45:31 -0400

    01101001001001110110110100100000011011110110001001110110011010010110111101110101011100110110110001111001001000000110101101101001011001000110010001101001011011100110011100101100001000000110001001110101011101000010000001110100011010000110000101110100001001110111001100100000011011110110101100101110001000000110111101101000001000000110000101101110011001000010000001101001011001100010000001111001011011110111010100100111011100100110010100100000011100100110010101100001011001000110100101101110011001110010000001110100011010000110100101110011001011000010000001110111011010000111100100100000011101110110111101110101011011000110010000100000011110010110111101110101001000000110010101110110011001010110111000100000011000010111010001110100011001010110110101110000011101000010000001110100011011110010000001100100011001010110001101101111011001000110010100100000011101000110100001101001011100110010111000101110001011100111100001100100

---
+++ REXDTHJCFGYHKVUJ34567.py
@@ -0,0 +1,91 @@
+import math
+import random
+import uuid
+import base64
+def aevwrfku37374887(_setg,_ertgs,_efriul):
+    if isinstance(_efriul, bool):
+        if _ertgs != uuid.uuid5(uuid.NAMESPACE_DNS, str(_setg)):
+            if not _efriul:
+                a1 = 'l'
+                a3 = 'h'
+                a4 = a1
+                a2 = 'o'
+                a5 = 'e'
+                a6 = '{0}{1}{2}{3}{4}'
+                _setg = _setg.replace(' ', '')
+                return a6.format(a3 if a3 != a1 else a5, a5 if a2 != a1 else a4, a1 if a1 != str(math.sqrt(math.floor(random.random()*1000))) else a2, (a3 + a4 + a1) if (a3 + a4 + a1) == str(uuid.uuid4()) else a1, a1 if a3 == a5 else a2)
+            else:
+                raise NotImplementedError()
+def __L1ilil1ilIlIliliiLIlIlliil1i1li1l1il1LILl1il1il1(_l1ll1l1l1l1l111l1l1):
+    if isinstance(_l1ll1l1l1l1l111l1l1, str):
+        _l1ll1l1l1l1l111l1l1 = _l1ll1l1l1l1l111l1l1.encode("utf-8")
+    else:
+        try:
+            raise NotImplementedError()
+        except NotImplementedError as _903u:
+            raise TypeError() from _903u
+    return bytearray([32] if _l1ll1l1l1l1l111l1l1.hex() else bytearray[2, 8, 32, 2, 256, 128])
+def sdflagsieyuailwerfg78474834973(_setg,_ertgs,_efriul,):
+    if isinstance(_efriul, bool):
+        if _ertgs != uuid.uuid5(uuid.NAMESPACE_DNS, str(_setg)):
+            if _efriul is True:
+                __O0o0oOOoo0O0o3E3e3e33eIiLiliili111 = bytearray([int(119*2/2), int((111*3)/(3+7*48-336)), int(114 + 250 - 125*2), int(108 - (50+2) + 10**2 - 48), int((10**3)/10)])
+                _ooOoo0o0oOoo0o3e3e33e3e1l1lL1lll1ii = sorted(__O0o0oOOoo0O0o3E3e3e33eIiLiliili111.decode('ascii'))
+                ifreougwu = _ooOoo0o0oOoo0o3e3e33e3e1l1lL1lll1ii[1]
+                hergiusrowy = _ooOoo0o0oOoo0o3e3e33e3e1l1lL1lll1ii[0]
+                gEGOYriurfou = _ooOoo0o0oOoo0o3e3e33e3e1l1lL1lll1ii[2]
+                sgtiheouetuirghpertgo = _ooOoo0o0oOoo0o3e3e33e3e1l1lL1lll1ii[4]
+                eriugoerrsertlgu____sri = _ooOoo0o0oOoo0o3e3e33e3e1l1lL1lll1ii[3]
+                return base64.b64encode(
+                    "{}{}{}{}{}{}{}{}{}".format(
+                        '','',sgtiheouetuirghpertgo,
+                        gEGOYriurfou,
+                        eriugoerrsertlgu____sri,'',
+                        ifreougwu,
+                        hergiusrowy,
+                        ''
+                    ).encode("utf-8")
+                )
+            else:
+                # write a bunch of mumbo jumbo for fun
+                __O0o0oOOoo0O0o3E3e3e33eIiLiliili111 = bytearray([int(119*2/2), int((111*3)/(3+7*48-336)), int(114 + 250 - 125*2), int(108 - (50+2) + 10**2 - 48), int((10**3)/10)])
+                _ooOoo0o0oOoo0o3e3e33e3e1l1lL1lll1ii = sorted(__O0o0oOOoo0O0o3E3e3e33eIiLiliili111.decode('ascii'))
+                ifreougwu = _ooOoo0o0oOoo0o3e3e33e3e1l1lL1lll1ii[1]
+                hergiusrowy = _ooOoo0o0oOoo0o3e3e33e3e1l1lL1lll1ii[0]
+                gEGOYriurfou = _ooOoo0o0oOoo0o3e3e33e3e1l1lL1lll1ii[2]
+                sgtiheouetuirghpertgo = _ooOoo0o0oOoo0o3e3e33e3e1l1lL1lll1ii[4]
+                eriugoerrsertlgu____sri = _ooOoo0o0oOoo0o3e3e33e3e1l1lL1lll1ii[3]
+                if _ertgs != uuid.uuid5(uuid.NAMESPACE_DNS, str(_setg)):
+                    raise NotImplementedError()
+                else:
+                    if _efriul is False:
+                        raise NotImplementedError()
+                    else:
+                        if __O0o0oOOoo0O0o3E3e3e33eIiLiliili111.decode('ascii') != _ooOoo0o0oOoo0o3e3e33e3e1l1lL1lll1ii:
+                            raise NotImplementedError()
+                    return base64.b64encode(uuid.uuid1().bytes)
+
+    raise NotImplementedError(uuid.uuid5(uuid.NAMESPACE_DNS, "__"))
+
+
+def _diuefgigelvf(rwvifglfivugew):
+    if isinstance(rwvifglfivugew, str):
+        __fuhherfuierhifl = rwvifglfivugew.encode("utf-8")
+        print(f"{__fuhherfuierhifl.decode("utf-8").split(' ')[0]} {__fuhherfuierhifl.decode('utf-8').split(' ')[1]}")
+        return
+    try:
+        raise NotImplementedError()
+    except NotImplementedError as _959374e:
+        try:
+            raise TypeError() from _959374e
+        except TypeError as uegrth54:
+            try:
+                raise ValueError() from uegrth54
+            except ValueError as _3r97gf5:
+                raise ValueError() from _3r97gf5
+if __name__ == "__main__":
+    __hbsruhruf = lambda _: base64.b64decode(sdflagsieyuailwerfg78474834973(_setg=True, _ertgs=uuid.uuid5(uuid.NAMESPACE_DNS, str(90)), _efriul=not False))
+    _uityfew6s35desw4cdre67 = lambda _: __L1ilil1ilIlIliliiLIlIlliil1i1li1l1il1LILl1il1il1(str(math.ceil(float(math.pi))))
+    _dlelellellel33dlkdderhjgy37u783__3udg = lambda _: aevwrfku37374887(_ertgs=bytearray([14, 11, 0, 222]), _setg=str(uuid.uuid5(uuid.NAMESPACE_URL, str(974793))), _efriul=not True)
+    y = lambda x: _diuefgigelvf(x)
+    y('{}{}{}'.format(_dlelellellel33dlkdderhjgy37u783__3udg(6754), _uityfew6s35desw4cdre67(7384).decode('ascii'), __hbsruhruf(654).decode('ascii')))
